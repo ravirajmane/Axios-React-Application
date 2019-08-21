0fa85bcd2bd7af655154fd3381ca5f20c21150b3
ravirajmane <myemail@gmail.com>
2019-08-20 18:48:17 -0700

    axios app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import axios from 'axios';
+
+export default class PersonList extends React.Component{
+  
+  constructor(){
+    super();
+    this.state={
+      name:'',
+      id:'',
+      persons:[]
+    }
+  }
+
+  handleChangeAdd=event=>{
+    this.setState({name:event.target.value});
+  }
+
+  handleChangeDelete = event => {
+    this.setState({ id: event.target.value });
+  }
+
+  handleSubmitAdd=event=>{
+    event.preventDefault();
+
+    let user={
+      name:this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+    .then(res=>{
+      console.log(res);
+      console.log(res.data);
+    })
+  }
+
+  handleSubmitDelete = event => {
+    event.preventDefault();
+
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res=>{
+      let persons=res.data;
+      this.setState({persons});
+    })
+  }
+
+
+
+render(){
+  let persons=this.state.persons;
+
+  return(
+    <div>
+    <ul>
+      {persons.map(person=><li>{person.name}</li>)}
+    </ul>
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Person Name:
+          <input type="text" name="name" onChange={this.handleChangeAdd}></input>
+        </label>
+        <button onClick={this.handleSubmitAdd}>Add</button>
+
+        <label>
+            Person ID:
+            <input type="text" name="id" onChange={this.handleChangeDelete} />
+          </label>
+          <button onClick={this.han}>Delete</button>
+
+      </form>
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+
+  }
